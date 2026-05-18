372d7cd83fd05eeb874856ae03487bf982146bfe
hxun202 <kangsojin0918@gmail.com>
2026-05-18 21:45:33 +0900

    2026.5.18 수출용 패키지

---
+++ Assets/Class3th (User Interface)/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Continue()
+    {
+        Debug.Log("Continue");
+    }
+
+    public void StartNewGame()
+    {
+        Debug.Log("Start");
+    }
+
+    public void OpenOption()
+    {
+        Debug.Log("Option");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Exit");
+    }
+}
