62e37b65072b81c6624f43e4b1fa5807edc9d410
hxun202 <kangsojin0918@gmail.com>
2026-05-22 21:43:45 +0900

    2026.5.22 시간 시스템

--- Assets/Class5th (Animator Controller)/Scripts/Animator Manager.cs
+++ Assets/Class5th (Animator Controller)/Scripts/Animator Manager.cs
@@ -3,25 +3,66 @@ using UnityEngine;
 public class AnimatorManager : MonoBehaviour
 {
     public Animator animator;
+    public AnimatorStateInfo animatorStateInfo;
 
     public void Die()
     {
+        animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (animatorStateInfo.IsName("Die") || animator.IsInTransition(0))
+        {
+            return;
+        }
+
         animator.SetTrigger("Die");
     }
 
         public void Attack()
         {
-        animator.SetBool("Attack", true);
+        animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (animatorStateInfo.IsName("Idle"))
+        {
+            animator.SetBool("Attack", true);
+            animator.SetBool("Idle", false);
+        }
+        else if (animatorStateInfo.IsName("Walk"))
+        {
+            animator.SetBool("Walk", false);
+            animator.SetBool("Idle", true);
+        }
         }
     
-        public void Walk()
+    public void Walk()
+    {
+        animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (animatorStateInfo.IsName("Attack"))
         {
-        animator.SetBool("Walk", true);
+            animator.SetBool("Walk", true);
+            animator.SetBool("Attack", false);
         }
+        else if(animatorStateInfo.IsName("Idle"))
+        {
+            animator.SetBool("Walk", true);
+            animator.SetBool("Idle", false);
+        }
+
+    }
     
     public void Idle()
     {
-        animator.SetBool("Walk", false);
-        animator.SetBool("Attack", false);
+        animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (animatorStateInfo.IsName("Walk"))
+        {
+            animator.SetBool("Walk",false);
+            animator.SetBool("Idle", true);
+        }
+        else if (animatorStateInfo.IsName("Attack"))
+        {
+            animator.SetBool("Idle", true);
+            animator.SetBool("Attack", false);
+        }
     }
 }
